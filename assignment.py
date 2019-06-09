18212a8f0b5ebcd7a31dad6fad9a8ae2e910af51
sarthak yogananda <sarthak16yogananda@gmail.com>
2019-06-09 22:16:02 +0530

    commit1

---
+++ assignment.py
@@ -0,0 +1,5 @@
+a=input("enter a number=")
+b=a.split(",")
+c=tuple(b)
+print(b)
+print(c)
---
+++ assignment1.py
@@ -0,0 +1,3 @@
+n=int(input("enter a three digit number="))
+sum=n%10+(n/10)%10+(n/100)
+print("sum of digits=",sum)
